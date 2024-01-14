e705f781bcdd059e8aa90f53e8df5cde4c49335a
singard <mathieugonza3@gmail.com>
2024-01-14 17:47:22 +0100

    add spring project

---
+++ unlimited-copies/src/main/java/com/singard/unlimitedcopies/UnlimitedCopiesApplication.java
@@ -0,0 +1,13 @@
+package com.singard.unlimitedcopies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlimitedCopiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlimitedCopiesApplication.class, args);
+	}
+
+}
---
+++ unlimited-copies/src/test/java/com/singard/unlimitedcopies/UnlimitedCopiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singard.unlimitedcopies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlimitedCopiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
