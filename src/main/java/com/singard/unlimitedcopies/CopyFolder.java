e417fe7c20bb8ff9f894852e2a92b99a8510ecbc
gonza <gonza@LAPTOP-Q6VLQI4T>
2024-01-16 15:18:05 +0100

    copy folder ok

--- src/main/java/com/singard/unlimitedcopies/CopyFolder.java
+++ src/main/java/com/singard/unlimitedcopies/CopyFolder.java
@@ -7,26 +7,33 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class CopyFolder {
 
 	public void copyFolder(String sourceFolder, String destinationFolder) throws IOException {
 		// Convertir les chemins en objets Path
 		Path sourcePath = Paths.get(sourceFolder);
 		Path destinationPath = Paths.get(destinationFolder);
-
+log.info("copy start");
 		// Crée le dossier de destination s'il n'existe pas
 		Files.createDirectories(destinationPath);
 
 		// Copie le contenu du dossier source vers le dossier destination
 		Files.walk(sourcePath, FileVisitOption.FOLLOW_LINKS)
+		
 		.forEach(source -> {
 			try {
 				Path destination = destinationPath.resolve(sourcePath.relativize(source));
 				Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+				log.info("copy finish");
 			} catch (IOException e) {
+				log.error("copy error",e);
 				e.printStackTrace(); // Gérer les exceptions appropriées selon vos besoins
 			}
 		});
--- src/main/java/com/singard/unlimitedcopies/StartupRunner.java
+++ src/main/java/com/singard/unlimitedcopies/StartupRunner.java
@@ -1,5 +1,6 @@
 package com.singard.unlimitedcopies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StartupRunner implements ApplicationRunner  {
 
+	@Autowired
+    private CopyFolder copyFolder;
+	
+	@Autowired
+    private FileTransfer fileTransfer;
+
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 	
         log.info("Application running");
+        
+        // Spécifiez le chemin du dossier source et du dossier destination
+        String sourceFolder = fileTransfer.getSourceFolderPath();
+        String destinationFolder = fileTransfer.getDestinationFolderPath();
+
+        // Appelez le service de copie
+        copyFolder.copyFolder(sourceFolder, destinationFolder);
 	}
 	 
 	
